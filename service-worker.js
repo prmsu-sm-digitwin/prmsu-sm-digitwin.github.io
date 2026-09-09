728a33ae38031414ca3d139230438f64070ef9d7
prmsu-sm-digitwin <digitaltwinprmsusm@gmail.com>
2026-09-10 07:16:59 +0800

    fix canteen pathway

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Update version in CACHE_NAME whenever files change
 
-const CACHE_NAME = 'prmsu-digitwin-v2.21'; // Update this version whenever files change 
+const CACHE_NAME = 'prmsu-digitwin-v2.22'; // Update this version whenever files change 
 
 const STATIC_ASSETS = [
   './',
