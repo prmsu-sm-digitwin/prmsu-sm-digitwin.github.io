01ef89b67aea53818774a0279f2bab0bcbfa8f25
prmsu-sm-digitwin <digitaltwinprmsusm@gmail.com>
2026-09-10 04:39:04 +0800

    welp

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Update version in CACHE_NAME whenever files change
 
-const CACHE_NAME = 'prmsu-digitwin-v2.17'; // Update this version whenever files change 
+const CACHE_NAME = 'prmsu-digitwin-v2.18'; // Update this version whenever files change 
 
 const STATIC_ASSETS = [
   './',
