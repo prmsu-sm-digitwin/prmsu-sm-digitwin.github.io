57e3a98655c978a097c98408b1a688caf954d64b
prmsu-sm-digitwin <digitaltwinprmsusm@gmail.com>
2026-09-11 20:59:08 +0800

    updated nav map models

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Update version in CACHE_NAME whenever files change
 
-const CACHE_NAME = 'prmsu-digitwin-v2.56'; // Update this version whenever files change 
+const CACHE_NAME = 'prmsu-digitwin-v2.57'; // Update this version whenever files change 
 
 const STATIC_ASSETS = [
   './',
